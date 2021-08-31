973ac9815f813df92b207e6f03935ce9076599d9
diwasdh <diwasdhakal75@gmail.com>
2021-08-31 10:17:50 +0545

    main

---
+++ dist/js/index.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+	//options here
+    navigation:true,
+    anchors: ['page1', 'page2', 'page3', 'page4'],
+	autoScrolling:true,
+	scrollHorizontally: false
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
